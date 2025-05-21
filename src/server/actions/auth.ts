fe86adbf91ee5169e77aa42c4ea055acc34eca98
Hilman <117459721+Hilmarch27@users.noreply.github.com>
2025-05-21 16:19:45 +0700

    fix: redirectTo

--- src/server/actions/auth.ts
+++ src/server/actions/auth.ts
@@ -6,6 +6,7 @@ import { signIn } from "../auth";
 export async function SignInMail(formData: FormData) {
   await signIn("nodemailer", {
     email: formData.get("email"),
+    redirectTo: "/settings",
   });
 
   revalidatePath("/sign-in");
